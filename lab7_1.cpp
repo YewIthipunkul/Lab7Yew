388338901d8fe8e758824b9871b1532165c5bc53
Yew <yewit247@gmail.com>
2024-12-03 14:33:16 +0700

    fix

---
+++ lab7_1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    double a  =  20 , b = 30;
+    double c = 4;
+    double output;
+    output = a - (b/c);
+    cout << "output: "<< output << "\n";
+    return 0;
+}
